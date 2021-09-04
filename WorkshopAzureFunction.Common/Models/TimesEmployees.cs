57326c84e8ffd0855078e2f7ebc2a27da7b0758f
Harold Stiven Pinto Gomez <hpinto@bancolombia.com.co>
2021-09-04 15:05:56 -0500

    Add Entity

---
+++ WorkshopAzureFunction.Common/Models/TimesEmployees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkshopAzureFunction.Common.Models
+{
+    public class TimesEmployees
+    {
+        public int IdEmployee { get; set; }
+        public DateTime DateInputOutput { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
---
+++ WorkshopAzureFunction.Functions/Entities/TimesEmployeesEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace WorkshopAzureFunction.Functions.Entities
+{
+    public class TimesEmployeesEntity : TableEntity
+    {
+        public int IdEmployee { get; set; }
+        public DateTime DateInputOutput { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
